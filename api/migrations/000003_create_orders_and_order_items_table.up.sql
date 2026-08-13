5305ab7f8abe8a58bc603b22a3870a85f591af59
Akmal Dzakwan Faiz <akmal.dz.f@gmail.com>
2026-08-14 06:52:13 +0700

    feat(db): add orders and order_items tables
    Create new database migrations to implement the orders and order_items
    tables. The orders table tracks user purchases, status, and total
    amounts, while the order_items table manages individual products
    within each order, including quantity and pricing details.

---
+++ api/migrations/000003_create_orders_and_order_items_table.up.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS orders (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL,
+    status TEXT NOT NULL CHECK (
+        status IN ('pending', 'paid', 'cancelled', 'completed')
+    ) DEFAULT 'pending',
+    total_amount BIGINT NOT NULL CHECK (total_amount >= 0),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    CONSTRAINT fk_orders_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS order_items(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    order_id UUID NOT NULL,
+    product_id UUID NOT NULL,
+    quantity INT NOT NULL CHECK (quantity > 0),
+    price BIGINT NOT NULL CHECK (price >= 0),
+    subtotal BIGINT NOT NULL CHECK (subtotal >= 0),
+
+    CONSTRAINT fk_order_items_order_id FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
+    CONSTRAINT fk_order_items_product_id FOREIGN KEY(product_id) REFERENCES products(id)
+);
